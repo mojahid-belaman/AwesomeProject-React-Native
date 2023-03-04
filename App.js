6c904a737f12f29b8f142d3b836069c8a678a7a3
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-04 16:13:53 +0100

    check eneterdText is empty or fill

--- App.js
+++ App.js
@@ -7,11 +7,17 @@ export default function App() {
   const [goalList, setGoalList] = useState([]);
 
   function goalAddHandler(enteredText, setEnteredText) {
-    enteredText && setGoalList((currentGoal) => [
-      ...currentGoal,
-      { text: enteredText, id: Math.random().toString() },
-    ]);
-    setEnteredText('');
+    enteredText &&
+      setGoalList((currentGoal) => [
+        ...currentGoal,
+        { text: enteredText, id: Math.random().toString() },
+      ]);
+    setEnteredText("");
+  }
+
+  function goalDeleteHandler(id) {
+    console.log(id);
+    setGoalList((currentGoal) => currentGoal.filter((item) => item.id !== id));
   }
   return (
     <View style={styles.appContainer}>
@@ -19,7 +25,9 @@ export default function App() {
       <View style={styles.goalContainer}>
         <FlatList
           data={goalList}
-          renderItem={(goal) => <GoalItem text={goal.item.text} />}
+          renderItem={(goal) => (
+            <GoalItem item={goal.item} goalDeleteHandler={goalDeleteHandler} />
+          )}
           keyExtractor={(item, idx) => item.id}
         />
       </View>
